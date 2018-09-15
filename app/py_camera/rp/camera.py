00e5569fdb4d4d639b754d864fd5d78dd4856b96
Pavel Durov <pdurov0@gmail.com>
2018-09-15 11:14:45 +0100

    recording video and cnvertig to mp4 vefore upload

--- app/py_camera/rp/camera.py
+++ app/py_camera/rp/camera.py
@@ -24,16 +24,16 @@ class Camera(object):
             self.cam = picamera.PiCamera()
         else:
             self.cam = MockedCamera();
-        self.set_config() 
+        self.set_config()
         self.action = None
-    
+
     def dispose(self):
         self.cam.close()
 
     def set_config(self):
         self.cam.resolution = self.CAM_RESOLUTION
         self.cam.framerate = self.CAM_FRAMERATE
-    
+
     def capture(self, num):
         result = []
         for i in range(0, num):
@@ -50,25 +50,37 @@ class Camera(object):
 
         self.logger.info('capture result {}'.format(result))
         return result
-    
+
     def __normalize_rec_time(self, sec):
         result = sec
         if sec < self.RECORDING_TIME_MIN_SEC:
             result = self.RECORDING_TIME_MIN_SEC
         return result
-     
+
+    def convert(self, path):
+        try:
+            convertedPath = path.replace("h264", "mp4")
+            cmd="MP4Box -fps 20 -add {} {} ".format(path, convertedPath)
+            self.logger.info('RUNNING H263 to MP4 convetion')
+            self.logger.info(cmd)
+            os.system(cmd)
+            return convertedPath
+        except:
+            self.logger.error('Error on h264-mp4 convertion {}'.format(sys.exc_info()))
+
     def video(self, sec):
         path = None
         try:
-            path = fs.generate_MP4_absolute_file_name(fs.VIDEO_GENERAL)
+            path = fs.generate_H264_absolute_file_name(fs.VIDEO)
             sec = self.__normalize_rec_time(sec)
             self.logger.info('recording started, {}, {} sec'.format(path, sec))
             self.cam.start_recording(path)
             self.cam.wait_recording(sec)
             self.cam.stop_recording()
+            path = self.convert(path)
             if not RP_CONTEXT:
                 path = '/home/ubuntu/workspace/app/media/test/big_buck_bunny.mp4'
-                
+
         except:
             self.logger.error(sys.exc_info())
 
